89ffb2837fb16c24c061c1412b2846c9c7a8a132
rmouwafaq <github@agilorg.com>
2014-10-14 11:57:44 +0000

    support to_dict pour les classes report_def

--- report_def.py
+++ report_def.py
@@ -1,3 +1,14 @@
+
+from openerp.osv import fields, osv
+import ho.pisa as pisa
+import os
+import time
+import netsvc
+import datetime
+from tools.translate import _
+
+    
+    
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
@@ -20,8 +31,6 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
-import ho.pisa as pisa
-import os
 import time
 import netsvc
 import datetime
@@ -33,20 +42,74 @@ class ir_module(osv.osv):
                 'report_def_ids':fields.one2many('report.def','module_id','Reports Definition'),
                 }
 ir_module()
+
+class res_company(osv.osv):  
+    _inherit="res.company" 
     
+    def to_dict(self,cr,uid,id=None):
+        
+        dict_company={}
+        id = self.search(cr,uid, [('id','=',id)])
+        
+        if id:
+            company = self.browse(cr,uid, id)[0]  
+            dict_company['id']=company.id
+            
+            dict_company['name']=(company.partner_id).to_dict()
+            
+            return dict_company
+        else:
+            return dict_company
+
+class res_users(osv.osv):  
+    _inherit="res.users" 
     
+    def to_dict(self,cr,uid,id=None):
+        
+        dict_user={}
+        id = self.search(cr,uid, [('id','=',id)])
+        
+        if id:
+            user = self.browse(cr,uid, id)[0]  
+            dict_user['id']=user.id
+            dict_user['partner']=(user.partner_id).to_dict()
+            dict_user['company']=(user.company_id).to_dict()
+            
+            return dict_user
+        else:
+            return dict_user
+
+class res_partner(osv.osv):  
+    _inherit="res.partner" 
+    
+    def to_dict(self,cr,uid,id=None):
+        
+        dict_partner={}
+        id = self.search(cr,uid, [('id','=',id)])
+        
+        if id:
+            partner = self.browse(cr,uid, id)[0]  
+            dict_partner['id']=partner.id
+            dict_partner['name']=partner.name
+            
+            return dict_partner
+        else:
+            return dict_partner
+
 class report_def(osv.osv):
     _name = "report.def"
     _description = "Agilorg report Definition"
     _columns = { 
                 'name': fields.char('Report Name', size=64, required=True, select=True),
                 'title': fields.char('Title', size=128, required=True, select=True),
-                'module_id':fields.many2one('ir.module.module', 'Module', required=True),
+                'module_id':fields.many2one('ir.module.module', 'Module',required=True),
                 'query' : fields.text('Query'),
                 'format':fields.selection([('Portrait', 'Portrait'),
                                            ('Landscape','Landscape')],
                                            'Format'),
-                    
+                'type':fields.selection([('normal', 'Normal'),
+                                           ('form','Formulary')],
+                                           'Type'),  
                 'template_html':fields.text('HTML script'),  
                 'template_file_name': fields.char('Template File Name', size=128, required=True, select=True),
                 'json_file_name': fields.char('JSON File Name', size=128, required=True, select=True),
@@ -54,15 +117,50 @@ class report_def(osv.osv):
                 'total_ids':fields.one2many('report.def.field.total','report_id','Report Totals'),
                 'section_bloc_ids':fields.one2many('report.section.bloc','report_id','Sections'),
                 } 
-#    def hello_func(self, cr, uid,html_content,context=None):
-#        pisa.showLogging() 
-#        filename = os.getcwd() + "/openerp/addons/report_def_store/static/pdf/testo.pdf"
-#        pdf = pisa.CreatePDF(html_content,file(filename, "wb+"))
+    
+    def to_dict(self,cr,uid,name=None,id=None):
         
+        dict_report={}
+        if name:
+            id = self.search(cr,uid, [('name','=',name)])
+        else:
+            id = self.search(cr,uid, [('id','=',id)])
+        
+        if id:
+            report = self.browse(cr,uid, id)[0]  
+            dict_report['id']=report.id
+            dict_report['name']=report.name
+            dict_report['title']=report.title
+            dict_report['module_id']=report.module_id.id
+            dict_report['query']=report.query
+            dict_report['format']=report.format
+            dict_report['type']=report.type
+            dict_report['template_html']=report.template_html
+            dict_report['template_file_name']=report.template_file_name
+            dict_report['json_file_name']=report.json_file_name
             
             
-#        return "ok"
+            dict_report['col_fields']=[]
+            for field_id in report.field_ids:
+                dict_report['col_fields'].append(field_id.to_dict())
+            
+            dict_report['col_totals']=[]
+            for total_id in report.total_ids:
+                dict_report['col_totals'].append(total_id.to_dict())
+                
+            dict_report['col_sections']=[]
+            for section_bloc_id in report.section_bloc_ids:
+                dict_report['col_sections'].append(section_bloc_id.to_dict())
+            
+            return dict_report
+        else:
+            return dict_report         
+
+    
+    
+    
 report_def()
+        
 
 class report_section_bloc(osv.osv):
     _name = "report.section.bloc"
@@ -81,9 +179,26 @@ class report_section_bloc(osv.osv):
     
     _defaults = {'max_bloc_number': 1 }
     
+    def to_dict(self,cr,uid,id=None):
+        
+        dict_report_section={}
+        
+        id = self.search(cr,uid, [('id','=',id)])
+        
+        if id:
+            report_section = self.browse(cr,uid, id)[0] 
+            dict_report_section['id']=report_section.id
+            dict_report_section['report_id']=report_section.report_id.id
+            dict_report_section['section']=report_section.section
+            dict_report_section['max_bloc_number']=report_section.max_bloc_number
+            
+            return dict_report_section
+        else:
+            return dict_report_section   
         
 report_section_bloc()
-        
+
+
 class report_def_field(osv.osv):
     
     _name = "report.def.field"
@@ -121,8 +236,7 @@ class report_def_field(osv.osv):
                                         ('Static Image','Static Image'),
                                         ],'Field Type'),
         'expression':fields.text('Expression'),
-        'group':fields.boolean("Groupe"),     
-        
+        'group':fields.boolean("Grouped")        
         
     }    
     
@@ -132,7 +246,35 @@ class report_def_field(osv.osv):
                  
                  }
                  
+    def to_dict(self,cr,uid,id):
+        
+        dict_report_def_field={}
+        
+        id = self.search(cr,uid, [('id','=',id)])
+        
+        if id:
+            report_def_field = self.browse(cr,uid, id)[0]  
+            
+            dict_report_def_field['id']=report_def_field.id
+            dict_report_def_field['template_id']=report_def_field.template_id
+            dict_report_def_field['report_id']=report_def_field.report_id.id
+            dict_report_def_field['name']=report_def_field.name
+            if report_def_field.total_id :
+                dict_report_def_field['total_id']=report_def_field.total_id.id
+            else:
+                dict_report_def_field['total_id']=0
+            dict_report_def_field['sequence']=report_def_field.sequence
+            dict_report_def_field['section']=report_def_field.section
+            dict_report_def_field['source_data']=report_def_field.source_data
+            dict_report_def_field['field_type']=report_def_field.field_type
+            dict_report_def_field['expression']=report_def_field.expression
+            
+            return dict_report_def_field
+        else:
+            return dict_report_def_field   
+                 
 report_def_field()
+
 
 class report_def_field_total(osv.osv):
     _name = "report.def.field.total"
@@ -148,16 +290,68 @@ class report_def_field_total(osv.osv):
                 'reset_repeat_section':fields.boolean('Reset for repeated section'),
                 'field_ids':fields.one2many('report.def.field','total_id','Total Fields'),
     }    
+    
+    
+    def to_dict(self,cr,uid,id=None):
+        
+        dict_report_def_field_total={}
+        
+        id = self.search(cr,uid, [('id','=',id)])
+        
+        if id:
+            report_def_field_total = self.browse(cr,uid, id)[0]  
+            dict_report_def_field_total['id']=report_def_field_total.id
+            dict_report_def_field_total['name']=report_def_field_total.name
+            dict_report_def_field_total['report_id']=report_def_field_total.report_id.id
+            dict_report_def_field_total['function']=report_def_field_total.function
+            dict_report_def_field_total['reset_after_print']=report_def_field_total.reset_after_print
+            dict_report_def_field_total['reset_repeat_section']=report_def_field_total.reset_repeat_section
+            
+            dict_report_def_field_total['col_fields']=report_def_field_total.field_ids
+            
+            return dict_report_def_field_total
+        else:
+            return dict_report_def_field_total 
+          
 report_def_field_total()
+
+
 class report_def_json_files(osv.osv):
+
     _name = "report.def.json_files"
     _description="Agilorg - Report definition files name json"
     _columns ={
                'name':fields.char('Json Name',size=100,required=True),
                'report_id':fields.many2one('report.def','Report Definition'),
                }
+
+    def to_dict(self,cr,uid,name=None,id=None):
+        
+        dict_report={}
+        if name:
+            id = self.search(cr,uid, [('name','=',name)])
+        else:
+            id = self.search(cr,uid, [('id','=',id)])
+        
+        if id:
+            report = self.browse(cr,uid, id)[0]  
+            dict_report['name']=report.name
+            dict_report['title']=report.titile
+            dict_report['module']=report.module_id.name
+            dict_report['query']=report.query
+            dict_report['format']=report.format
+            dict_report['template_html']=report.template_html
+            dict_report['template_file_name']=report.template_file_name
+            #dict_report['col_fields']=
+            #dict_report['col_totals']=
+            #dict_report['section_bloc_ids']=
+            
+            return dict_report
+        else:
+            return dict_report   
     
 report_def_json_files()
+
 
 
 
